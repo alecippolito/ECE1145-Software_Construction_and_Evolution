ce0a70dfecef0a66c178fabc6663545bbad7b697
Reece Basehore <60199333+REB143@users.noreply.github.com>
2022-10-02 17:22:53 -0400

    GammaCiv changes

--- starter-code-ece1145 - Copy/src/main/java/hotciv/framework/Unit.java
+++ starter-code-ece1145 - Copy/src/main/java/hotciv/framework/Unit.java
@@ -1,36 +1,32 @@
 package hotciv.framework;
 
 /** Represents a single unit in the game.
+ Responsibilities:
+ 1) Know its type name.
+ 2) Know its owner.
+ 2) Know its defensive and attacking strengths.
+ This source code is from the book
+ "Flexible, Reliable Software:
+ Using Patterns and Agile Development"
+ published 2010 by CRC Press.
+ Author:
+ Henrik B Christensen
+ Department of Computer Science
+ Aarhus University
 
-Responsibilities:
-1) Know its type name.
-2) Know its owner.
-2) Know its defensive and attacking strengths.
+ Please visit http://www.baerbak.com/ for further information.
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
 
-   This source code is from the book 
-     "Flexible, Reliable Software:
-       Using Patterns and Agile Development"
-     published 2010 by CRC Press.
-   Author: 
-     Henrik B Christensen 
-     Department of Computer Science
-     Aarhus University
-   
-   Please visit http://www.baerbak.com/ for further information.
+ http://www.apache.org/licenses/LICENSE-2.0
 
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
- 
-       http://www.apache.org/licenses/LICENSE-2.0
- 
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-
-*/
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
 public interface Unit {
 
   /** return the type of the unit
@@ -53,16 +49,27 @@ public interface Unit {
    * @return the move count
    */
   public int getMoveCount();
-  
+
   /** return the defensive strength of this unit
    * @return defensive strength
    */
   public int getDefensiveStrength();
-  
+
   /** return the attack strength of this unit
    * @return attack strength
    */
   public int getAttackingStrength();
 
+  public void action();
+
   public void setUnitOwner(Player p);
+
+  public void resetMove();
+
+  public Unit getUnitDef();
+
+  public boolean checkMove();
+
+  public void moved();
 }
+
