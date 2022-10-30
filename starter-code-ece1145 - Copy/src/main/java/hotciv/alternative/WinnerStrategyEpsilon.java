47a3bcf384ad7e6ad4aa5bada8819461bccc6740
Reece Basehore <60199333+REB143@users.noreply.github.com>
2022-10-29 20:05:01 -0400

    Added WinnerStrategyEpsilon

---
+++ starter-code-ece1145 - Copy/src/main/java/hotciv/alternative/WinnerStrategyEpsilon.java
@@ -0,0 +1,24 @@
+package hotciv.alternative;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import hotciv.framework.Player;
+import hotciv.framework.Tile;
+import hotciv.framework.WinnerStrategy;
+
+public class WinnerStrategyEpsilon implements WinnerStrategy {
+    public Player getWinner(int age, ArrayList<Tile> cityTiles, HashMap<Player, Integer> winHashMap, int roundNumber) {
+        Player winner = null;
+        for(Player key : winHashMap.keySet()) {
+            if(winHashMap.get(key) >= 3){
+                winner = key;
+            }else{}
+        }
+        return winner;
+
+    }
+
+    public void countAttack(HashMap<Player, Integer> attackCounts, Player player) {
+        attackCounts.put(player,attackCounts.get(player)+1);
+    }
+}
